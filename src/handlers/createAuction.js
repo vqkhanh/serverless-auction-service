7e999545f06d7986916e2b881a871cb9bdf3a701
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-07-09 16:12:43 +0700

    create IAM role

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,30 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+//contex: contain metadata, provider when lambda executed
+// event: object inlude all infomation about event or this execution like body query parameter, path para, header, so on
+// can add custom data (event or context) via middleware
 async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    id: uuid(),
+    title,
+    status: 'OPEN',
+    createAt: now.toISOString()
+  };
+
+  await dynamoDB.put({
+    TableName: 'AuctionsTable',
+    Item: auction,
+  }).promise();
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    statusCode: 201,
+    body: JSON.stringify({ auction }),
   };
 }
 
